23cd917d4327d38c4a818a35aaaaf6e1e0928a2c
VictoryCATiii3 <victor7670236@gmail.com>
2025-01-16 12:38:37 -0800

    Made the output nice and fancy

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ def main():
     file_contents = get_book_contents(book_path)
     #print(file_contents)
     word_count = get_word_count(file_contents)
-    print(word_count)
-
+    print(f"{word_count} words were found in the document")
     letter_counts = count_letters(file_contents)
-    print(letter_counts)
+
+    print_count(letter_counts)
 
 def get_book_contents(book_path):
     with open(book_path) as f:
@@ -26,5 +26,11 @@ def count_letters(str_in):
         else:
             letter_counts[letter] = 1
     return letter_counts
+
+def print_count(letter_counts):
+    sorted_count = sorted(letter_counts.items(), key=lambda item:item[1], reverse=True)
+    for item in sorted_count:
+        if item[0].isalpha():
+            print(f"The '{item[0]}' character was found {item[1]} times")
 
 main()
